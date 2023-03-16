ce9f0a9ce2b3a4a9a0bbe812c782921d8edb1df3
Sunflower081 <128060981+Sunflower081@users.noreply.github.com>
2023-03-16 20:41:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Fashion Stylist", "Bommer", "Introvert", "Boomer", "Singer", "Sunflower!",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
